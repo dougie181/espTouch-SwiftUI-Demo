ad7f25910ef5235bbdfa07f1710eb87a1e4ee3b2
dougie181 <doug@wwp.com.au>
2020-10-30 11:29:15 +1000

    Initial Commit

---
+++ esp32SmartConfigDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  esp32SmartConfigDemo
+//
+//  Created by Doug Inman on 30/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ esp32SmartConfigDemo/esp32SmartConfigDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  esp32SmartConfigDemoApp.swift
+//  esp32SmartConfigDemo
+//
+//  Created by Doug Inman on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct esp32SmartConfigDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
